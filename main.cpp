5a45f01e65631de248a44de3685d8feb75afb031
Pray Chow <1007950881@qq.com>
2019-09-10 18:32:59 +0800

    C++ Data type & Literal constant

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "我是\"pray chow!\"\n" << endl;
+
+    cout << "形态\t大小(bytes)\n";
+    cout << "int\t" << sizeof(int) << "\n";
+    cout << "int16\t" << sizeof(int16_t) << "\n";
+    cout << "int8\t" << sizeof(int8_t) << "\n";
+    cout << "int32\t" << sizeof(int32_t) << "\n";
+    cout << "int64\t" << sizeof(int64_t) << "\n";
+    cout << "long\t" << sizeof(long) << "\n";
+    cout << "float\t" << sizeof(float) << "\n";
+    cout << "double\t" << sizeof(double) << "\n";
+    cout << "char\t" << sizeof(char) << "\n" << endl;
+
+    cout << "sizeof(bool)\t" << sizeof(bool) << endl;
+    cout << "sizeof(true)\t" << sizeof(true) << endl;
+    cout << "sizeof(false)\t" << sizeof(false) << endl;
+    cout << "true\t" << static_cast<int>(true) << endl;
+    cout << "false\t" << static_cast<int>(false) << "\n" << endl;
+
+    cout << 26 << "\n"; // 十进制
+    cout << 032 << "\n"; // 八进制
+    cout << 0x1A << "\n"; // 十六进制
+
+    return 0;
+}
