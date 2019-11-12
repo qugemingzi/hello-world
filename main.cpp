ead8a6550927db056f45abf758e68e459e44d8c5
Pray Chow <1007950881@qq.com>
2019-11-12 13:40:33 +0800

    learn c++

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "math.h"
 #include<cstdarg>
 #include "sort.h"
+#include "Ball.h"
 
 using namespace std;
 
@@ -792,6 +793,20 @@ int main()
     }
     cout << endl;
 
+    // class
+    Ball ball1;
+    cout << ball1.name() << "\t" << ball1.volumn() << endl;
+
+    ball1.name("green ball");
+    ball1.radius(2.5);
+    cout << ball1.name() << "\t" << ball1.volumn() << endl;
+
+    Ball ball2(5.0, "black ball");
+    cout << ball2.name() << "\t" << ball2.volumn() << endl;
+
+    string name("yellow ball");
+    Ball ball3(10.0, name);
+    cout << ball3.name() << "\t" << ball3.volumn() << endl;
 
     return 0;
 }
